3a7fdacf4c54b406e6d2e7ff1b329cdcdc98963c
kauboimausu <malagonngasca@gmail.com>
2025-07-24 12:17:21 -0600

    Added ship class
    Created ship class, as well as its necessary attributes and methods to know how many times it's been hit, whether it's been destroyed and to increase the number of hits it has received

---
+++ src/ship.js
@@ -0,0 +1,16 @@
+class Ship {
+    constructor(length) {
+        this.length = length;
+        this.damaged = 0;
+    }
+
+    hit() {
+        this.damaged++;
+    }
+
+    isSunk() {
+        return (this.damaged == this.length);
+    }
+}
+
+export default Ship;
