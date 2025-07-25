dcbbc3b9006126c34d639a654b616c9cdbb46a88
kauboimausu <malagonngasca@gmail.com>
2025-07-24 23:55:45 -0600

    Finished adding placeShip functionality
    Made all the tests for placeShip pass properly, added throw clashing ships error when putting a ship over another

--- src/gameboard.js
+++ src/gameboard.js
@@ -19,6 +19,13 @@ class GameBoard {
             // We'll check to see if the ship is being placed out of bounds, we'll throw an error if it is
             if(xCoordinate + shipLength > 9) {
                 throw new Error("Out of Bounds");
+            } else {
+                // Otherwise we'll check that there's not ships in the way, if there are we'll throw out an error
+                for(let index = xCoordinate; index < xCoordinate + shipLength; index++) {
+                    if(this.squares[yCoordinate][index].ship != null){
+                        throw new Error("Clashing Ships");
+                    }
+                }
             }
             // Then we'll change the ship values for each of the squares the ship is placed in
             for(let index = xCoordinate; index < xCoordinate + shipLength; index++){
@@ -30,6 +37,12 @@ class GameBoard {
             // We'll do pretty much the same here
             if(yCoordinate - shipLength < 0) {
                 throw new Error("Out of Bounds");
+            } else {
+                for(let index = yCoordinate; index > yCoordinate - shipLength; index--) {
+                    if(this.squares[index][xCoordinate].ship != null){
+                        throw new Error("Clashing Ships");
+                    }
+                }
             }
             for(let index = yCoordinate; index > yCoordinate - shipLength; index--){
                 this.squares[index][xCoordinate].ship = ship;
