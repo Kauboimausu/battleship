89ec4c6e59a422402f37859d66814b44252fd6e4
kauboimausu <malagonngasca@gmail.com>
2025-07-25 14:38:57 -0600

    Created player.js file
    Created the player javascript file, also added the necessary constructor for it, I think that's all the class needs for now

---
+++ src/player.js
@@ -0,0 +1,13 @@
+const GameBoard = require("./gameboard");
+
+
+class Player {
+    constructor(name ="Odin", real=false) {
+        this.name = name;
+        // If the player is real it'll be the human player, otherwise it's the computer
+        this.real = real;
+        this.board = new GameBoard();
+    }
+}
+
+module.exports = Player;
